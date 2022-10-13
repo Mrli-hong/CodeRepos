f0679b2032665f8093546e40a316d14b93533028
LeeHongkong <10927126+leehongkong@user.noreply.gitee.com>
2022-10-13 10:21:58 +0800

    second commit

---
+++ LoggingDemo1/NLogTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingDemo1
+{
+    class NLogTest
+    {
+        private readonly ILogger<NLogTest> logger;
+        public NLogTest(ILogger<NLogTest> logger)
+        {
+            this.logger = logger;
+        }
+        public void Test()
+        {
+            logger.LogDebug("数据库");
+            logger.LogWarning("数据库还行");
+            logger.LogInformation("你好");
+            logger.LogCritical("数据库炸了");
+            try
+            {
+                File.ReadAllText("A:/1.txt");
+                logger.LogDebug("读取文件成功！");
+            }
+            catch (Exception ex)
+            {
+                //两个参数的LogDebug
+                logger.LogDebug(ex,"读取文件失败");
+            }
+        }
+    }
+}
---
+++ LoggingDemo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+      
+            //注册日志服务，并增加配置AddConsole
+            services.AddLogging(loggerBuilder=> {
+                loggerBuilder.AddConsole();
+                //设置最低显示级别
+                loggerBuilder.SetMinimumLevel(LogLevel.Trace);
+            });
+
+            services.AddScoped<NLogTest>();
+
+            using (var sp = services.BuildServiceProvider())
+            {
+                var log = sp.GetService<NLogTest>();
+                log.Test();
+            }
+        }
+    }
+}
