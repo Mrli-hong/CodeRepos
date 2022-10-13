ed55f8fb197242e7c215a69eb348f22bad162fcb
LeeHongkong <10927126+leehongkong@user.noreply.gitee.com>
2022-10-13 16:30:40 +0800

    Serilog

--- LoggingDemo1/Program.cs
+++ LoggingDemo1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
 using System;
 
 namespace LoggingDemo1
@@ -17,10 +20,19 @@ namespace LoggingDemo1
                 //增加包Microsoft.Extensions.Logging.EventLog
                 //loggerBuilder.AddEventLog();
 
-                loggerBuilder.AddNLog();
+                //采用NLog进行输出，相关输出的内容在nlog.config文件中进行配置
+                //loggerBuilder.AddNLog();
+                
+                //采用Serilog进行格式化输出
+                Log.Logger = new LoggerConfiguration()
+                             .MinimumLevel.Debug()
+                             .Enrich.FromLogContext()
+                             .WriteTo.Console(new JsonFormatter())
+                             .CreateLogger();
+                loggerBuilder.AddSerilog();
 
                 //设置最低显示级别
-                loggerBuilder.SetMinimumLevel(LogLevel.Trace);
+                //loggerBuilder.SetMinimumLevel(LogLevel.Trace);
             });
 
             services.AddScoped<NLogTest>();
